a32680dbec5d68f0a7b5abf93b49f0697a15b4e0
valflrt <vfleurit@gmail.com>
2021-05-03 15:23:45 +0200

    ...

--- music.js
+++ music.js
@@ -1,9 +1,21 @@
-const ytdl = require("youtube-dl");
+const ytdl = require("ytdl-core");
 
-const add = (args) => {
+let queue = new Array();
+
+class Song {
+	async constructor(content) {
+		let { videoDetails } = await ytdl.getInfo(content);
+		this.name = videoDetails.title;
+		this.url = videoDetails.video_url;
+		this.videoDetails = videoDetails;
+	}
+}
+
+const add = async (args) => {
 	let { message, content } = args;
 
-	ytdl.getInfo(content);
+	new Song(content);
+
 };
 
-module.exports = {}
+module.exports = { add };
