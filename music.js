dda986f71a7270a48e828967e5d74cb6d022f057
valflrt <vfleurit@gmail.com>
2021-05-17 11:24:34 +0200

    ...

--- music.js
+++ music.js
@@ -146,6 +146,7 @@ const stopMusic = async (args) => {
 const skipSong = async (args) => {
 	let { message } = args;
 	if (guilds.has(message.guild.id) && guilds.get(message.guild.id).voiceDispatcher) {
+		guilds.get(message.guild.id).next();
 		play(args);
 	} else message.embed(`Tu dois d'abord démarrer la musique pour la passer !`);
 };
